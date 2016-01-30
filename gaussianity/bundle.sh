24ba178524dcd49fb038dcf10acc1effca2042ac
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-01-30 17:48:01 +0100

    ...

--- gaussianity/bundle.sh
+++ gaussianity/bundle.sh
@@ -15,3 +15,10 @@ cat $inp | grep '<script' | grep src | sed 's@.* src="\([^"]*\).*@\1@g' \
 | while read i ; do
     cp ../$i .
 done
+cat $inp | grep '<link rel' | grep src | sed 's@.* href="\([^"]*\).*@\1@g' \
+| while read i ; do
+    cp ../$i .
+done
+
+sed -i -e 's@\(.*script.* src="\)[^"]*/\([^"]*".*\)@\1\2@g' $inp
+sed -i -e 's@\(.*link rel=.* href="\)[^"]*/\([^"]*".*\)@\1\2@g' $inp
