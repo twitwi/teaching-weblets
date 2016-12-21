d7e8b5f894f7548de53711a90a22696c4f4081eb
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-12-21 17:13:39 +0100

    Adding a color associated to the value

--- convolution/logic.js
+++ convolution/logic.js
@@ -11,9 +11,13 @@ var start = function() {
     var tx = document.getElementById('lastvalue');
 
     var showValue = function(x, y, cx, cy) {
-        tx.textContent = data[y][x].toFixed(2);
+        var v = data[y][x];
+        var vcolor = (v - dataMin) / (dataMax - dataMin);
+        var color = hsv2rgb(200 - 200*vcolor, 1, 1);
+        tx.textContent = v.toFixed(2);
         tx.style.left = cx+"px";
         tx.style.top = (cy-20)+"px";
+        tx.style.color = color;
     };
 
     var onMove = function(e) {
@@ -30,3 +34,42 @@ var start = function() {
     
 };
 start();
+
+
+
+// https://gist.github.com/schinckel/1588489#file-hsv2rgb-js
+
+var hsv2rgb = function(h,s,v) {
+  //var h = hsv.hue, s = hsv.sat, v = hsv.val;
+  var rgb, i, data = [];
+  if (s === 0) {
+    rgb = [v,v,v];
+  } else {
+    h = h / 60;
+    i = Math.floor(h);
+    data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
+    switch(i) {
+      case 0:
+        rgb = [v, data[2], data[0]];
+        break;
+      case 1:
+        rgb = [data[1], v, data[0]];
+        break;
+      case 2:
+        rgb = [data[0], v, data[2]];
+        break;
+      case 3:
+        rgb = [data[0], data[1], v];
+        break;
+      case 4:
+        rgb = [data[2], data[0], v];
+        break;
+      default:
+        rgb = [v, data[0], data[1]];
+        break;
+    }
+  }
+  return '#' + rgb.map(function(x){ 
+    return ("0" + Math.round(x*255).toString(16)).slice(-2);
+  }).join('');
+};
