b4d1a250755d4b9908d1463f540a3fb40187a12d
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-12-21 16:52:33 +0100

    Nicer current value display

--- convolution/logic.js
+++ convolution/logic.js
@@ -10,17 +10,23 @@ var start = function() {
     var fi = document.getElementById('filter');
     var tx = document.getElementById('lastvalue');
 
-    var showValue = function(x, y) {
-        tx.textContent = data[y][x];
+    var showValue = function(x, y, cx, cy) {
+        tx.textContent = data[y][x].toFixed(2);
+        tx.style.left = cx+"px";
+        tx.style.top = (cy-20)+"px";
     };
 
     var onMove = function(e) {
-        fi.style.left = (e.clientX-fi.width/2)+"px";
-        fi.style.top = (e.clientY-fi.height/2)+"px";
-        showValue(e.clientX, e.clientY);
+        var x = e.clientX - fi.width/2;
+        var y = e.clientY - fi.height/2;
+        fi.style.left = x+"px";
+        fi.style.top = y+"px";
+        showValue(e.clientX, e.clientY, x, y);
     };
+    
     im.addEventListener('mousemove', onMove);
     fi.addEventListener('mousemove', onMove);
+    tx.addEventListener('mousemove', onMove);
     
 };
 start();
