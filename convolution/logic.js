0e0056ab98051b208fd3edf42418b008087d3ea0
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-12-21 18:29:04 +0100

    Mass exploration and proper scrolling

--- convolution/logic.js
+++ convolution/logic.js
@@ -5,42 +5,77 @@ var start = function() {
     var data = rawConvData.values;
     var dataMin = rawConvData.min;
     var dataMax = rawConvData.max;
-    
+
+    var base = document.getElementById('cont');
     var im = document.getElementById('image');
     var fi = document.getElementById('filter');
     var tx = document.getElementById('lastvalue');
+    var mod = document.getElementById('model');
     var cvs = document.getElementById('cvs');
 
     cvs.width = im.width;
     cvs.height = im.height;
     cvs.style.left = im.width+"px";
+    mod.style.left = im.width+"px";
+
+    var marvelous = false;
     
-    var showValue = function(x, y, cx, cy) {
+    var showValue = function(x, y, boxx, boxy) {
         var v = data[y][x];
         var vcolor = (v - dataMin) / (dataMax - dataMin);
         var color = hsv2rgb(200 - 200*vcolor, 1, 1);
+
         tx.textContent = v.toFixed(2);
-        tx.style.left = cx+"px";
-        tx.style.top = (cy-20)+"px";
+        tx.style.left = boxx+"px";
+        tx.style.top = (boxy-20)+"px";
         tx.style.color = color;
 
         var ctx = cvs.getContext("2d");
         ctx.fillStyle = color;
-        ctx.fillRect(cx, cy, 1, 1);
-        
+        ctx.fillRect(x, y, 1, 1);
+
+        if (marvelous) {
+            revealSomePointsAround(x, y, 50, 2000);
+        }
+
+    };
+
+    var revealSomePointsAround = function(basex, basey, ran, nbp) {
+        var ctx = cvs.getContext("2d");
+        var w = im.width;
+        var h = im.height;
+
+        for (var i = 0; i<nbp; i++) {
+            var x = basex + getRandomIntInclusive(-ran, ran);
+            var y = basey + getRandomIntInclusive(-ran, ran);
+            if (x>=0 && y>=0 && x<w && y<h) {
+                var v = data[y][x];
+                var vcolor = (v - dataMin) / (dataMax - dataMin);
+                var color = hsv2rgb(200 - 200*vcolor, 1, 1);
+                ctx.fillStyle = color;
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
     };
 
     var onMove = function(e) {
-        var x = e.clientX - fi.width/2;
-        var y = e.clientY - fi.height/2;
-        fi.style.left = x+"px";
-        fi.style.top = y+"px";
-        showValue(e.clientX, e.clientY, x, y);
+        marvelous = e.shiftKey;
+        var x = e.pageX;
+        var y = e.pageY;
+        var boxx = x - fi.width/2;
+        var boxy = y - fi.height/2;
+        fi.style.left = boxx+"px";
+        fi.style.top = boxy+"px";
+        showValue(x, y, boxx, boxy);
     };
     
     im.addEventListener('mousemove', onMove);
     fi.addEventListener('mousemove', onMove);
     tx.addEventListener('mousemove', onMove);
+    fi.addEventListener('click', function(e) {
+        revealSomePointsAround(e.clientX, e.clientY, 20, 1000);
+        revealSomePointsAround(e.clientX, e.clientY, 50, 1000);
+    });
     
 };
 start();
@@ -83,3 +118,10 @@ var hsv2rgb = function(h,s,v) {
     return ("0" + Math.round(x*255).toString(16)).slice(-2);
   }).join('');
 };
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
